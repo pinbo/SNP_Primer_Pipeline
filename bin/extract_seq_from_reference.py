5ad9f3beba2ff4725af33bf453414676d64eb64c
Junli Zhang <zhjl86@gmail.com>
2019-05-26 16:55:11 -0700

    updated extract_seq because BLAST+ update to version 2.9.0+

--- bin/extract_seq_from_reference.py
+++ bin/extract_seq_from_reference.py
@@ -21,46 +21,12 @@
 #  MA 02110-1301, USA.
 #  
 #
+## Update on 2019-05-26 because BLAST+ update to version 2.9.0+
+## I do not need add the range to the output, the default output already has it.
 
 from subprocess import call
 import sys
 
-# function to get the flanking sequences
-def get_flanking(range_file, flanking_file):
-	reference = "/Library/WebServer/Documents/blast/db/nucleotide/IWGSC_v2_ChrU.fa"
-	cmd = "blastdbcmd -entry_batch " + range_file + " -db " + reference + " > " + flanking_file
-	print "Command to get the flanking sequences for each SNP\n", cmd
-	call(cmd, shell=True)
-
-# read input file
-def get_seq_name(infile):
-	seq_name_list = []
-	for line in open(infile):
-		line = line.strip()
-		if not line: # skip blank lines
-			continue
-		seq_name_list.append(line.replace("\t", " "))
-	return seq_name_list
-
-
-# function to extract sequences from a fasta file 
-def get_fasta(infile, seq_name_list):
-	fasta = {} # dictionary for alignment
-	n = 0 # for sequence name list index
-	with open(infile) as file_one:
-		for line in file_one:
-			line = line.strip()
-			if not line:
-				continue
-			if line.startswith(">"):
-				sequence_name = line.split()[0].lstrip(">")
-				if sequence_name in seq_name_list[n]: # in case seq name mismatch
-					sequence_name = seq_name_list[n]
-					n += 1
-			else:
-				fasta.setdefault(sequence_name, "")
-				fasta[sequence_name] += line.rstrip()
-	return fasta
 
 
 def main(args):
@@ -70,22 +36,11 @@ def main(args):
 	"/Volumes/DATA2/databases/ncbi/nucleotide/Triticum_aestivum.TGACv1.dna.toplevel.fa",
 	"/Users/galaxy/blastdb/IWGSC_v1.1_HC_20170706_cds.fasta"]
 	infile = args[1]
-	outfile = args[2]
-	reference = reference_list[int(args[3]) - 1] # reference 1 or 2
+	reference = reference_list[int(args[2]) - 1] # reference 1 or 2
 	# step 1: get the flanking sequences
 	flanking_file = "temp_flanking_seq.fa"
 	cmd = "blastdbcmd -entry_batch " + infile + " -db " + reference + " > " + flanking_file
 	call(cmd, shell=True)
-	# step 2: replace the fasta sequence names with more information
-	seq_name_list = get_seq_name(infile)
-	seq_fasta = get_fasta(flanking_file, seq_name_list)
-	# step 3: write the organized fasta
-	out = open(outfile, "w")
-	for i in seq_name_list:
-		out.write(">" + i + "\n")
-		out.write(seq_fasta[i] + "\n")
-	out.close()
-	
 	return 0
 
 if __name__ == '__main__':
